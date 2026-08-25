bb01fcbb5da8e039350b0073c35070d6a52929ef
Ytallo Brito De Oliveira <147071008+ytallobritooliveira@users.noreply.github.com>
2026-08-24 21:42:35 -0300

    Add /usuarios endpoint to return user list

--- scr/index.js
+++ scr/index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
     res.send("API FUNCIONANDO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 });
 
-app.get('/', (req,res) => {
+app.get('/usuarios', (req,res) => {
     res.json([
         {id: 1, nome: 'joão'},
         {id: 2, nome: 'Maria'}
