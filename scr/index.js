127301c3b02508c018294ce86dd3c8c442bfc69c
Ytallo Brito De Oliveira <147071008+ytallobritooliveira@users.noreply.github.com>
2026-08-24 21:38:28 -0300

    Add API route for testing functionality

--- scr/index.js
+++ scr/index.js
@@ -2,9 +2,15 @@ const express = require('express')
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("API FUNCIONANDO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+});
+
 app.get('/', (req,res) => {
     res.json([
         {id: 1, nome: 'joão'},
         {id: 2, nome: 'Maria'}
     ])
 })
+
+module.exports = app;
